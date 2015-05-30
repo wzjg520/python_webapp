2d3bdc726644f42e6564a1995a1797479eba9aa7
john <john@wzjg520.com>
2015-05-30 13:09:15 +0800

    db_module

---
+++ www/transwarp/db.py
@@ -0,0 +1,321 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+__author__ = 'John Wang'
+
+'''
+底层数据库操作模块
+'''
+
+import time
+import uuid
+import functools
+import threading
+import logging
+
+
+class Dict(dict):
+
+    def __init__(self, names=(), value=(), **kw):
+        super(dict, self).__init__(**kw)
+
+        for k, v in zip(names, value):
+            self[k] = v
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r'dict 没有属性 %s ' % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+
+def next_id(t=None):
+    '''
+    return 50-char string as next_id
+
+    Args:
+            t:unix timestamp
+    '''
+
+    if not t:
+        t = time.time()
+    return '%000d%s000' % (int(t * 1000), uuid.uuid4().hex)
+
+# 日志信息
+
+
+def _profilling(start, sql=''):
+    t = time.time() - start
+    if t > 0.1:
+        logging.warning('[PROFILING] [DB] %s: %s' % (t, sql))
+    else:
+        logging.info('[PROFILING] [DB] %S: %S' % (t, sql))
+
+
+class DBError(Exception):
+    pass
+
+
+class MultiColumnsError(DBError):
+    pass
+
+
+class _LasyConnection(object):
+
+    def __init__(self):
+        self.connection = None
+
+    # 获得游标
+    def cursor(self):
+        if self.connection is None:
+            connection = engine.connect()
+            logging.info('open connection <%s>...' % hex(id(connection)))
+            self.connection = connection
+
+        return self.connection.cursor()
+    # 提交事务
+
+    def commit(self):
+        self.connection.commit()
+
+    # 事务回滚
+    def rellback(self):
+        self.connection.rollback()
+
+    # 清理现场
+    def cleanup(self):
+        if self.connection:
+            connection = self.connection
+            self.connection = None
+            logging.info('close connection <%s>...' % hex(id(connection)))
+            connection.close()
+
+# 当前数据库连接的上下文环镜(线程)
+
+
+class _DbCtx(threading.local):
+
+    def __init__(self):
+        self.connection = None
+        # transactions 为0时关闭数据库
+        self.transactions = 0
+
+    def is_init(self):
+        return not self.connection is None
+
+    def init(self):
+        logging.info('open lazy connection...')
+        self.connection = _LasyConnection()
+
+    def cleanup(self):
+        self.connection.cleanup()
+        self.connection = None
+
+    def cursor(self):
+        return self.connection.cursor()
+
+# 独立的线程数据库
+_db_ctx = _DbCtx()
+
+# global
+engine = None
+
+# 创建mysql
+
+
+class _Engine(object):
+
+    def __init__(self, connect):
+        self._connect = connect
+
+    def connect(self):
+        return self._connect()
+
+
+def create_engine(user, password, database, host='127.0.0.1', port=3306, **kw):
+    import mysql.connector
+    global engine
+    if engine is not None:
+        raise DBError('Engine is already initalized.')
+    params = dict(
+        user=user, password=password, database=database, host=host, port=port)
+    defaults = dict(use_unicode=True, charset='utf8',
+                    collation='utf8_general_ci', autocommit=False)
+    for k, v in defaults.iteritems():
+        params[k] = kw.pop(k, v)
+    params.update(kw)
+    params['buffered'] = True
+
+    engine = _Engine(lambda: mysql.connector.connect(**params))
+
+    # 测试连接
+    logging.info('Init mysql engine <%s> ok' % hex(id(engine)))
+
+
+class _ConnectionCtx(object):
+
+    def __enter__(self):
+        global _db_ctx
+        self.should_cleanup = False
+        if not _db_ctx.is_init():
+            _db_ctx.init()
+            self.should_cleanup = True
+        return self
+
+    def __exit__(self, exctype, excvalue, traceback):
+        global _db_ctx
+        if self.should_cleanup:
+            _db_ctx.cleanup()
+
+
+def connection():
+    return _ConnectionCtx()
+
+
+def with_connection(func):
+    @functools.wraps(func)
+    def _wrapper(*args, **kw):
+        with _ConnectionCtx():
+            return func(*args, **kw)
+    return _wrapper
+
+
+class _TransactionCtx(object):
+
+    def __enter__(self):
+        global _db_ctx
+        self.should_close_conn = False
+        if not _db_ctx.is_init():
+            _db_ctx.init()
+            self.should_close_conn = True
+        _db_ctx.transactions = _db_ctx.transactions + 1
+        logging.info('begin transactions...' if _db_ctx.transactions ==
+                     1 else 'join current transaction...')
+        return self
+
+    def __exit__(self, exctype, excvalue, traceback):
+        global _db_ctx
+        _db_ctx.transactions = _db_ctx.transactions - 1
+        try:
+            if _db_ctx.transactions == 0:
+                if exctype is None:
+                    self.commit()
+                else:
+                    self.rollback()
+        finally:
+            if self.should_close_conn:
+                _db_ctx.cleanup()
+
+    def commit(self):
+        global _db_ctx
+        logging.info('commit transaction...')
+        try:
+            _db_ctx.connection.commit()
+            logging.info('commit ok.')
+        except:
+            logging.warning('commit failed. try rollback...')
+            _db_ctx.connection.rollback()
+            logging.warning('rollback ok.')
+            raise
+
+    def rollback(self):
+        global _db_ctx
+        logging.warning('rollback transaction...')
+        _db_ctx.connection.rollback()
+        logging.info('rollback ok.')
+
+
+def transaction():
+    return _TransactionCtx()
+
+
+def with_transaction(func):
+    @functools.wraps(func)
+    def _wrapper(*args, **kw):
+        _start = time.time()
+        with _TransactionCtx():
+            return func(*args, **kw)
+        _profiling(_start)
+    return _wrapper
+
+
+def _select(sql, first, *args):
+    global _db_ctx
+    cursor = None
+    sql = sql.replace('?', '%s')
+    logging.info('SQL: %s, ARGS: %s' % (sql, args))
+    try:
+        cursor = _db_ctx.connection.cursor()
+        cursor.execute(sql, args)
+        if cursor.description:
+            names = [i[0] for i in cursor.description]
+        if first:
+            values = cursor.fetchone()
+            if not values:
+                return None
+            return Dict(names, values)
+        return [Dict(names, i) for i in cursor.fetchall()]
+    finally:
+        if cursor:
+            cursor.close()
+
+
+@with_connection
+def select_one(sql, *args):
+    return _select(sql, True, *args)
+
+
+@with_connection
+def select_int(sql, *args):
+    d = _select(sql, true, *args)
+    if len(d) != 1:
+        raise MultiColumnsError('Expect only one column.')
+    return d.values()[0]
+
+
+@with_connection
+def select(sql, *args):
+    return _select(sql, False, *args)
+
+
+@with_connection
+def _update(sql, *args):
+    global _db_ctx
+    cursor = None
+    sql = sql.replace('?', '%s')
+    logging.info('SQL: %s, ARGS: %s' % (sql, args))
+    try:
+        cursor = _db_ctx.connection.cursor()
+        cursor.execute(sql, args)
+        r = cursor.rowcount
+        if _db_ctx.transactions == 0:
+            logging.info('auto commit')
+            _db_ctx.connection.commit()
+        return r
+    finally:
+        if cursor:
+            cursor.close()
+
+
+def insert(table, **kw):
+    cols, args = zip(*kw.iteritems())
+    sql = 'insert into 	`%s` (%s) values (%s)' % (table, ','.join(
+        ['`%s`' % col for col in cols]), ','.join(['?' for i in range(len(cols))]))
+    return _update(sql, *args)
+
+
+def update(sql, *args):
+    return _update(sql, *args)
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    create_engine('root', 'QQQQQQ', 'test')
+    update('drop table if exists user_web')
+    update(
+        'create table user_web (id char(40) primary key, name text, email text, passwd text, last_modified real)')
+
+    for i in range(100):
+    	insert('user_web',**{'id' : next_id(), 'name' : 'john' + str(i), 'email' : 'wzjg520@126.com','passwd' : 'xxx','last_modified' : time.time()} )
